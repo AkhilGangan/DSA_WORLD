979fb19b70bbb618e2a80acc688cac7252bf8d55
Akhil <akhilg7162@gmail.com>
2023-07-13 22:06:16 +0530

    Updated Files

---
+++ SumZeroNew.js
@@ -0,0 +1,29 @@
+//Checking sum zero - Problem 1
+//[-5,-4,-3,-2,0,2,4,6,8] -> input
+//[?,?] -> output
+//Another solution
+
+function FindSumPair(array){
+    let left = 0;
+    let right = array.length-1;
+
+    while(left < right){
+        sum=array[left]+array[right];
+        if (sum===0){
+            return [array[left],array[right]];
+        }
+        else if (sum>0){
+            right--;
+        }
+        else{
+            left++;
+        }
+    }
+}
+
+const result = FindSumPair([-5,-4,-3,-2,0,2,4,6,8]);
+console.log(result);
+
+// o(n) linear time complexity
+
+
